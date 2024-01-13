992c17e3c81f13b47a75d8923a135cf584e1e43e
rifkydarmawan62 <rifkydarmawan62@gmail.com>
2024-01-14 02:25:37 +0700

    Penambahan validasi

--- Buat_Certificate_Signing_Requests.py
+++ Buat_Certificate_Signing_Requests.py
@@ -14,21 +14,25 @@ else:
         print(f"{Fore.LIGHTBLUE_EX}Tekan Alt + Tab untuk membuka jendela baru")
         DIREKTORI_FILE_KUNCI_PRIVATE = pilih_file(title = "*Pilih file kunci private", filetypes = [("Privacy Enhanced Mail", "*.pem"), ("Distinguished Encoding Rules", "*.der"), ("Semua File", "*.*")])
         if DIREKTORI_FILE_KUNCI_PRIVATE:
-            DIREKTORI_FILE_CSR = simpan_file(title = "*Pilih lokasi file Certificate Signing Requests (CSR) disimpan", filetypes = [("Privacy Enhanced Mail", "*.pem")], defaultextension = ".pem", confirmoverwrite = True, initialfile = ".pem", initialdir = "/".join(DIREKTORI_FILE_KUNCI_PRIVATE.split("/")[:-1]))
-            if DIREKTORI_FILE_CSR:
-                print(f"{Fore.LIGHTGREEN_EX}File Kunci Private = \"{DIREKTORI_FILE_KUNCI_PRIVATE}\"\nFile CSR disimpan = \"{DIREKTORI_FILE_CSR}\"")
-                PERINTAH = f"openssl req -new -key \"{DIREKTORI_FILE_KUNCI_PRIVATE}\" -verbose -out \"{DIREKTORI_FILE_CSR}\""
-                print(f"{Fore.LIGHTYELLOW_EX}Menjalankan perintah [{PERINTAH}] ...{Fore.LIGHTBLUE_EX}")
-                try:
-                    run(PERINTAH, shell = True, check = True)
-                except CalledProcessError:
-                    print(f"{Fore.LIGHTRED_EX}Gagal membuat CSR!")
-                try:
-                    getpass(f"{Fore.RESET}Tekan Enter untuk keluar")
-                except KeyboardInterrupt:
-                    pass
+            try:
+                run(f"openssl pkey -check -in \"{DIREKTORI_FILE_KUNCI_PRIVATE}\"", shell = True, check = True, capture_output = True)
+            except CalledProcessError:
+                print(f"{Fore.RED}File kunci private \"{DIREKTORI_FILE_KUNCI_PRIVATE}\" tidak valid!{Fore.RESET}")
             else:
-                print(f"{Fore.LIGHTRED_EX}File CSR tidak disimpan!{Fore.RESET}")
+                DIREKTORI_FILE_CSR = simpan_file(title = "*Pilih lokasi file Certificate Signing Requests (CSR) disimpan", filetypes = [("Privacy Enhanced Mail", "*.pem")], defaultextension = ".pem", confirmoverwrite = True, initialfile = ".pem", initialdir = "/".join(DIREKTORI_FILE_KUNCI_PRIVATE.split("/")[:-1]))
+                if DIREKTORI_FILE_CSR:
+                    PERINTAH = f"openssl req -new -key \"{DIREKTORI_FILE_KUNCI_PRIVATE}\" -verbose -out \"{DIREKTORI_FILE_CSR}\""
+                    print(f"{Fore.LIGHTYELLOW_EX}Menjalankan perintah [{PERINTAH}] ...{Fore.LIGHTBLUE_EX}")
+                    try:
+                        run(PERINTAH, shell = True, check = True)
+                    except CalledProcessError:
+                        print(f"{Fore.LIGHTRED_EX}Gagal membuat CSR!")
+                    try:
+                        getpass(f"{Fore.RESET}Tekan Enter untuk keluar")
+                    except KeyboardInterrupt:
+                        pass
+                else:
+                    print(f"{Fore.LIGHTRED_EX}File CSR tidak disimpan!{Fore.RESET}")
         else:
             print(f"{Fore.LIGHTRED_EX}File kunci private tidak dipilih!{Fore.RESET}")
     except KeyboardInterrupt:
