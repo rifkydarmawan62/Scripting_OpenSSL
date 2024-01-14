ad199c6963b21f6fe5b385b6ecc2579d6df69bd2
rifkydarmawan62 <rifkydarmawan62@gmail.com>
2024-01-14 08:06:27 +0700

    File baru ditambahkan

---
+++ Konversi_Self_Signed_Certificate_ke_format_PEM.py
@@ -0,0 +1,36 @@
+from colorama import Fore
+from subprocess import run, CalledProcessError
+from tkinter.filedialog import askopenfilename as pilih_file
+from tkinter.messagebox import showerror
+from getpass import getpass
+from os.path import exists
+from os import remove
+
+print(f"{Fore.LIGHTYELLOW_EX}Memeriksa instalasi OpenSSL melalui CLI ...{Fore.LIGHTBLUE_EX}")
+try:
+    run("openssl version", shell = True, check = True)
+except CalledProcessError:
+    showerror("Gagal menjalankan aplikasi", "Perintah OpenSSL Tidak Ditemukan\nSilahkan download OpenSSL dari openssl.org dan atur konfigurasi file executable pada environment variable agar perintah OpenSSL dapat dijalankan")
+else:
+    try:
+        print(f"{Fore.LIGHTBLUE_EX}Tekan Alt + Tab untuk membuka jendela baru")
+        DIREKTORI_FILE_SERTIFIKAT = pilih_file(title = "*Pilih file self signed certificate", filetypes = [("Distinguished Encoding Rules", "*.der"), ("Semua File", "*.*")])
+        if DIREKTORI_FILE_SERTIFIKAT:
+            DIREKTORI_FOLDER = "/".join(DIREKTORI_FILE_SERTIFIKAT.split("/")[:-1])
+            NAMA_FILE = ".".join(DIREKTORI_FILE_SERTIFIKAT.split("/")[-1].split(".")[:-1]) if "." in DIREKTORI_FILE_SERTIFIKAT.split("/")[-1] else DIREKTORI_FILE_SERTIFIKAT.split("/")[-1]
+            PERINTAH = f"openssl x509 -in \"{DIREKTORI_FILE_SERTIFIKAT}\" -out \"{DIREKTORI_FOLDER}/{NAMA_FILE}.pem\" -outform PEM"
+            print(f"{Fore.LIGHTYELLOW_EX}Menjalankan perintah [{PERINTAH}] ...{Fore.LIGHTBLUE_EX}")
+            try:
+                run(PERINTAH, shell = True, check = True)
+            except CalledProcessError:
+                print(f"{Fore.LIGHTRED_EX}Konversi sertifikat ke file .pem gagal!")
+                if exists(f"{DIREKTORI_FOLDER}/{NAMA_FILE}.pem"):
+                    remove(f"{DIREKTORI_FOLDER}/{NAMA_FILE}.pem")
+            try:
+                getpass(f"{Fore.RESET}Tekan Enter untuk keluar")
+            except KeyboardInterrupt:
+                pass
+        else:
+            print(f"{Fore.LIGHTRED_EX}File sertifikat tidak dipilih!{Fore.RESET}")
+    except KeyboardInterrupt:
+        print(f"{Fore.LIGHTRED_EX}Interupsi Keyboard Ctrl + C!\nProgram ditutup{Fore.RESET}")
