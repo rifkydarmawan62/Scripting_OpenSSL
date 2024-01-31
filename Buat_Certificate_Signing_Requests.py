e2dfeebf2440cdd7353e5879f0aeff17b37ff18c
rifkydarmawan62 <rifkydarmawan62@gmail.com>
2024-01-31 11:09:59 +0700

    Optimalisasi Program

--- Buat_Certificate_Signing_Requests.py
+++ Buat_Certificate_Signing_Requests.py
@@ -5,7 +5,7 @@ from os.path import exists
 from os import remove
 from modul import bersihkan_layar
 
-bersihkan_layar(f"{Fore.LIGHTYELLOW_EX}Memeriksa instalasi OpenSSL ...{Fore.RESET}")
+bersihkan_layar(f"{Fore.YELLOW}Memeriksa instalasi OpenSSL ...{Fore.RESET}")
 try:
     run("openssl version", shell = True, check = True)
 except CalledProcessError:
@@ -13,30 +13,26 @@ except CalledProcessError:
 else:
     print(f"{Fore.LIGHTBLUE_EX}Tekan Alt + Tab untuk membuka jendela baru{Fore.RESET}")
     try:
-        DIREKTORI_FILE_KUNCI_PRIVATE = pilih_file(title = "*Pilih file kunci private", filetypes = [("Privacy Enhanced Mail", "*.pem"), ("Distinguished Encoding Rules", "*.der"), ("Semua File", "*.*")])
-        if DIREKTORI_FILE_KUNCI_PRIVATE:
-            PERINTAH_VALIDASI = f"openssl pkey -check -in \"{DIREKTORI_FILE_KUNCI_PRIVATE}\" -noout"
-            print(f"{Fore.YELLOW}Menjalankan perintah validasi {Fore.LIGHTYELLOW_EX}{Back.BLUE}{PERINTAH_VALIDASI}{Fore.YELLOW}{Back.RESET} ...{Fore.RESET}")
-            try:
-                run(PERINTAH_VALIDASI, shell = True, check = True)
-            except CalledProcessError:
-                print(f"{Fore.LIGHTRED_EX}Kunci private tidak valid!{Fore.RESET}")
-            else:
-                print(f"{Fore.LIGHTGREEN_EX}Kunci private valid!\n{Fore.LIGHTBLUE_EX}Tekan Alt + Tab untuk membuka jendela baru{Fore.RESET}")
-                DIREKTORI_FILE_CSR = simpan_file(title = "*Pilih lokasi file Certificate Signing Requests (CSR) disimpan", filetypes = [("Privacy Enhanced Mail", "*.pem")], defaultextension = ".pem", confirmoverwrite = True, initialfile = ".pem", initialdir = "/".join(DIREKTORI_FILE_KUNCI_PRIVATE.split("/")[:-1]))
-                if DIREKTORI_FILE_CSR:
-                    PERINTAH = f"openssl req -new -key \"{DIREKTORI_FILE_KUNCI_PRIVATE}\" -verbose -out \"{DIREKTORI_FILE_CSR}\""
-                    print(f"{Fore.YELLOW}Menjalankan perintah {Fore.LIGHTYELLOW_EX}{Back.BLUE}{PERINTAH}{Fore.YELLOW}{Back.RESET} ...{Fore.RESET}")
-                    try:
-                        run(PERINTAH, shell = True, check = True)
-                    except CalledProcessError:
-                        print(f"{Fore.LIGHTRED_EX}CSR gagal dibuat!{Fore.RESET}")
-                        if exists(DIREKTORI_FILE_CSR):
-                            remove(DIREKTORI_FILE_CSR)
-                    else:
-                        print(f"{Fore.LIGHTGREEN_EX}CSR dibuat di direktori \"{DIREKTORI_FILE_CSR}\"{Fore.RESET}")
+        INPUT_FILE_KUNCI_PRIVATE = pilih_file(title = "*Pilih file kunci private", filetypes = [("Privacy Enhanced Mail", "*.pem"), ("Distinguished Encoding Rules", "*.der"), ("Semua File", "*.*")])
+        if INPUT_FILE_KUNCI_PRIVATE:
+            OUTPUT_FILE_CSR = simpan_file(title = "*Pilih lokasi file Certificate Signing Requests (CSR) disimpan", filetypes = [("Privacy Enhanced Mail", "*.pem")], defaultextension = ".pem", confirmoverwrite = True, initialfile = ".pem", initialdir = "/".join(INPUT_FILE_KUNCI_PRIVATE.split("/")[:-1]))
+            if OUTPUT_FILE_CSR:
+                PERINTAH = f"openssl req -new -key \"{INPUT_FILE_KUNCI_PRIVATE}\" -verbose -out \"{OUTPUT_FILE_CSR}\""
+                print(f"{Fore.YELLOW}Menjalankan perintah {Fore.LIGHTYELLOW_EX}{Back.BLUE}{PERINTAH}{Fore.YELLOW}{Back.RESET} ...{Fore.RESET}")
+                try:
+                    run(PERINTAH, shell = True, check = True)
+                except CalledProcessError:
+                    print(f"{Fore.LIGHTRED_EX}CSR gagal dibuat!{Fore.RESET}")
+                    if exists(OUTPUT_FILE_CSR):
+                        remove(OUTPUT_FILE_CSR)
+                except KeyboardInterrupt:
+                    print(f"{Fore.LIGHTRED_EX}CSR batal dibuat!{Fore.RESET}")
+                    if exists(OUTPUT_FILE_CSR):
+                        remove(OUTPUT_FILE_CSR)
                 else:
-                    print(f"{Fore.LIGHTRED_EX}File CSR tidak disimpan!{Fore.RESET}")
+                    print(f"{Fore.LIGHTGREEN_EX}CSR dibuat di direktori \"{OUTPUT_FILE_CSR}\"{Fore.RESET}")
+            else:
+                print(f"{Fore.LIGHTRED_EX}File CSR tidak disimpan!{Fore.RESET}")
         else:
             print(f"{Fore.LIGHTRED_EX}File kunci private tidak dipilih!{Fore.RESET}")
     except KeyboardInterrupt:
